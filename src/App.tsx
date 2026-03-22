822c5f65270b0cfe4834beef1a56e857714fe8a9
emilielydiat <emilielydiat@gmail.com>
2026-03-22 00:18:03 +0000

    chore: render default Badge in App for demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Badge } from "./components/Badge/Badge";
+import { MockBadgeIcon } from "./components/Badge/MockBadgeIcon";
+
 export default function App() {
   return (
     <div className="p-4">
       <h1 className="text-lg font-semibold mb-2">Design system setup</h1>
-      <p className="text-sm text-gray-600">
+      <p className="text-sm text-gray-600 mb-2">
         Use Storybook to view, test and document the Badge component.
       </p>
-    </div>    
+      <Badge label="Add text" icon={<MockBadgeIcon />} />
+    </div>
   );
 }
